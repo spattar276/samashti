83cd63a91cf4a966e18ca753f3d0f2450683c527
Vibha G N <vnarayanareddy@walmartlabs.com>
2020-11-07 18:10:01 +0530

    Initial outline

---
+++ src/components/Menu.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import FullpageContext from  '@ap.cx/react-fullpage';
+
+class Menu extends FullpageContext {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {
+      menuItems = []
+    } = this.props;
+
+    const {
+      number, slides, transitionTiming,
+    } = this.context;
+
+    const gotoSlide = (slide) => {
+      const { goto } = this.context;
+      goto(slide);
+    };
+
+    if(!slides.length)
+      return null;
+
+    return (
+      <ul>
+        {
+          slides.map((slide, i) => (
+            <li
+              key={i.toString()}
+              style={{
+                transition: `all ${transitionTiming * 0.5}ms ease-in-out`,
+              }}
+              onClick={() => gotoSlide(slide)}
+              onKeyPress={() => gotoSlide(slide)}
+              aria-label={`Slide ${i}`}
+              className={number === i ? "active" : ""} >
+              { menuItems[i] }
+            </li>
+          ))
+        }
+      </ul>
+    );
+  }
+}
+
+export default Menu;

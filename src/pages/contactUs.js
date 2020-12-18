b302ba693030f1205472e96369527c5086e49969
soumya pattar <soumya.pattar@paytmmoney.com>
2020-12-18 23:49:49 +0530

    Contactus-page

--- src/pages/contactUs.js
+++ src/pages/contactUs.js
@@ -3,15 +3,19 @@ export default class OurWork extends Component {
   render() {
     return (
       <div className="container contact-us">
-        <h2 className="section-heading">Contact Us</h2>
         <div className="container-section">
           <div className="address">
             <h4>SAMASHTI FOUNDATION</h4>
+            <hr/>
             <p>Registered Office: 22, Embassy Tranqui, Alit#403, 8th Main 3rd Block, Koramangala, Bangalore</p>
-            <p>Email: samashtifoundation@yahoo.com, Samashti.india@gmail.com</p>
-            <p>Teliphone: 9663973295, 9663879295</p>
+            <p>Email : <strong> samashtifoundation@yahoo.com ,  Samashti.india@gmail.com </strong></p>
+            <p>Telephone :  <strong>9663973295, 9663879295 </strong></p>
+            <hr/>
             <div>
-              Follow us on <a target="_blank" href="https://www.facebook.com/samashtiindia">Facebook</a> <a target="_blank" href="https://www.instagram.com/samashtifoundation/">Instagram</a>
+              Follow us on 
+              <br/>
+              <p>Facebook: <a target="_blank" href="https://www.facebook.com/samashtiindia">Facebook</a></p>
+              <p>Instagram:<a target="_blank" href="https://www.instagram.com/samashtifoundation/">Instagram</a></p>
             </div>
           </div>
         </div>

d8ca130ce1c7728df16ccdff302eaacb431141c3
soumya pattar <soumya.pattar@paytmmoney.com>
2021-01-06 12:19:40 +0530

    our-work changes

--- src/pages/ourWork.js
+++ src/pages/ourWork.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import HorizontalTimeline from 'react-horizontal-timeline';
+import Video1 from '../images/video-1.mp4'
+import flowchart from '../images/howwework.png'
 //import 'react-vertical-timeline-component/style.min.css';
 // "Raise funds through individuals, CSR, community collaboration, Bank tieups for loans, p2p platforms and other business collaborations",
 const values = [
@@ -17,18 +19,37 @@ export default class OurWork extends Component {
   
   render() {
     return (
-      <div className="container our-work">
-        <h2>How we work</h2>
-        <div className="workflow-container ">  
-          {
-            values.map((item,index) => {
-              return (
-                <div><span>{++index}</span>{item}</div>
-              )
-            })
-          }
-      
-        </div>
+      <div className="wrapper">
+        <div className="container ">
+          <section className="our-work">
+            <h2>How we work</h2>
+            <div style={{width: '65%', height: '450px'} }>
+              <img style={{width: '100%', height: '100%', objectFit:"contain"} }src={flowchart} alt="how we work" />
+            </div>
+          </section>
+        {/*   <div className="workflow-container ">  
+            {
+              values.map((item,index) => {
+                return (
+                  <div><p>{++index}</p>{item}</div>
+                )
+              })
+            }
+          </div> */}
+          <section>
+            <h2>Our Projects</h2>
+            <div className="projects-container">
+              <h3>Village : Nijjiyappanadoddi, Ramanagaram |  Grassroot Organization : BERU </h3>
+              <div>
+                <video width="320" height="240" controls>
+                  <source src={Video1} type="video/mp4" />
+                </video>
+                <p>Sustainable Organic Farming Transition projects. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+          
+            
+            </div>
+          </section>
         
         
       
@@ -51,6 +72,7 @@ export default class OurWork extends Component {
             <p>In a nutshell, the village and farmers will be adopted and will be assisted through funding for sustainable organic farming and to improve farmers livelihood.</p>
           </div>
         </div> */}
+       </div> 
      </div>
     );
   }
